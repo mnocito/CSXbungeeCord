fa55732d4727e9a2af30f42b3e29ea4e4eac9283
Leyla Kamshad <c19lk@dalton.org>
2018-02-07 13:16:58 -0500

    not working

--- bungeeCordLKMN/BungeeSimulation.java
+++ bungeeCordLKMN/BungeeSimulation.java
@@ -17,21 +17,24 @@ public class BungeeSimulation extends AbstractSimulation {
 	double g = -9.81;
 	double n; //number of segments
 
-	
-	
-	
+
+
+
 	protected void doStep() {
 		if (positionY <=-40) {
 			boy.color = Color.green;
-			
+
 		} else {
+			for (int i = 0; i < bungee.masses.length; i++) {
+				bungee.masses[i].setY(bungee.masses[i].getY()+time*bungee.masses[i].getA());
+			}
 			boy.setY(positionY);
 			positionY += g*time;
 			bungeetrail.addPoint(0, positionY);
 		}
-				
+
 		time+=timestep;
-		
+
 	}
 	public void initialize() {
 		frame.setVisible(true);
@@ -39,11 +42,12 @@ public class BungeeSimulation extends AbstractSimulation {
 		frame.addDrawable(boy);
 		bungeetrail.addPoint(0, 0);
 		frame.setPreferredMinMax(-100, 100, -100, 100);
-		
+
 		for (int i = 0; i < n; i++) {
 			bungee.masses[i] = new Mass(bungee.getMass()/n, 0, g, (bungee.getLength()/n)*i);
+			frame.addDrawable(bungee.masses[i]);
 		}
-		
+
 	}
 	public static void main (String[] args) {
 		SimulationControl.createApp(new BungeeSimulation());
